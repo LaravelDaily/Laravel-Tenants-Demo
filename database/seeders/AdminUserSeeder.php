f4b85302e43fe13b64500bcf4fca3c05fd990c50
Povilas Korop <povilask007@gmail.com>
2020-11-12 14:27:50 +0200

    Admin user seeded

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('secret'),
+            'role_id' => 1
+        ]);
+    }
+}
